50bd2bcc3e0196792d7adc0880945d48cceffdec
unknown <miwanczuk7@gmail.com>
2016-04-09 00:51:51 +0200

    [TASK] Wyodrębnienie klasy GUI manager do zarządzania elementami gui, dodanie nagłówka z nazwami kolumn do pliku csv

--- GUIManager.py
+++ GUIManager.py
@@ -1,6 +1,171 @@
-
 import PyQt4.QtGui as QtGui
 import PyQt4.QtCore as QtCore
 
-class GUIManager():
-    pass
+class GUIManager(object):
+    # fixed height of all buttons in app
+    BUTTON_HEIGHT = 25
+    # fixed width of all buttons in app
+    BUTTON_WIDTH = 125
+
+    def __init__(self, parent):
+        self.parent = parent
+
+        # dict of keypoit line edits,they need to be members because we need to change their value
+        # when user clicks on image currently active keypoint line edit is filled with coordinates
+        # currently active keypoint is marked by variable current_keypoint
+        self.keypoints_line_edits = dict()
+        for keypoint in self.parent.KEYPOINT_NAMES: 
+            qLine = QtGui.QLineEdit() 
+            qLine.setDisabled(True)
+            self.keypoints_line_edits[keypoint] = qLine
+
+    
+
+    def init_widgets(self):
+        """ Initialize all widgets and put them in right possitions in the main window."""
+        # main box that holds all subboxes
+        main_box = QtGui.QGroupBox()
+        # layaut of main box 
+        main_layout = QtGui.QHBoxLayout()
+        
+        # setup box for buttons in leftmost panel
+        main_menu_group_box = QtGui.QGroupBox()
+        main_menu_layout = QtGui.QVBoxLayout()
+        self.init_main_menu_layout(main_menu_layout)
+        main_menu_group_box.setLayout(main_menu_layout)
+        main_layout.addWidget(main_menu_group_box)
+
+        # setup box keypoints menu
+        keypoints_menu_box =  QtGui.QGroupBox()
+        keypoints_menu_layout = QtGui.QVBoxLayout();
+        self.init_keypoint_menu_layout(keypoints_menu_layout)
+        keypoints_menu_box.setLayout(keypoints_menu_layout)
+        main_layout.addWidget(keypoints_menu_box)
+
+        # setup box for picure
+        picture_group_box =  QtGui.QGroupBox()
+        picture_layout = QtGui.QVBoxLayout();
+        self.init_picture_canvas_layout(picture_layout)
+        picture_group_box.setLayout(picture_layout)
+        main_layout.addWidget(picture_group_box)
+        
+        main_box.setLayout(main_layout)
+        self.parent.setCentralWidget(main_box)
+
+    def init_main_menu_layout(self, main_menu_layout):
+        """ Initiates main menu layout. """
+        ####
+        # image swap
+        ####
+          
+        img_swap_layout = QtGui.QHBoxLayout()
+        # next img
+        btn = QtGui.QPushButton("PREV", self.parent)
+        btn.clicked.connect(lambda: self.parent.change_potted_img(-1))
+        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
+        img_swap_layout.addWidget(btn)
+
+        # next img
+        btn = QtGui.QPushButton("NEXT", self.parent)
+        btn.clicked.connect(lambda: self.parent.change_potted_img(1))
+        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
+        img_swap_layout.addWidget(btn)
+
+        img_swap_box = QtGui.QGroupBox()
+        img_swap_box.setTitle("Change image")
+        img_swap_box.setLayout(img_swap_layout)
+        main_menu_layout.addWidget(img_swap_box)
+
+
+        #####
+        # person swap
+        #####
+        person_swap_layout = QtGui.QHBoxLayout()
+        
+        # prev person
+        btn = QtGui.QPushButton("PREV", self.parent)
+        btn.clicked.connect(lambda: self.parent.change_plotted_person(-1))
+        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
+        person_swap_layout.addWidget(btn)
+
+        spacerItem = QtGui.QSpacerItem(20, 30, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
+        main_menu_layout.addItem(spacerItem)
+
+        # next person
+        btn = QtGui.QPushButton("NEXT", self.parent)
+        btn.clicked.connect(lambda: self.parent.change_plotted_person(1))
+        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
+        person_swap_layout.addWidget(btn)
+
+        person_swap_box = QtGui.QGroupBox()
+        person_swap_box.setTitle("Change person")
+        person_swap_box.setLayout(person_swap_layout)
+        main_menu_layout.addWidget(person_swap_box)
+
+        spacerItem = QtGui.QSpacerItem(20, 30, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
+        main_menu_layout.addItem(spacerItem)
+
+        # buttons to save data to file
+        btn = QtGui.QPushButton("SAVE",self.parent)
+        btn.clicked.connect(self.parent.save_data_to_csv)
+        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
+        main_menu_layout.addWidget(btn)
+
+        # resets all keypoints on current image
+        btn = QtGui.QPushButton("RESET", self.parent)
+        btn.clicked.connect(self.parent.reset_all_current_img_keypoints)
+        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
+        main_menu_layout.addWidget(btn)
+
+        # quit app
+        btn = QtGui.QPushButton("QUIT", self.parent)
+        btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        main_menu_layout.addWidget(btn)
+
+        # spacer item to squeeze all buttons to the top
+        spacerItem = QtGui.QSpacerItem(20, 100, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        main_menu_layout.addItem(spacerItem)
+
+    def init_keypoint_menu_layout(self, main_keypoints_layout):
+        """ Initiates layout of keypoints menu."""
+        # add control buttons
+        controls_box = QtGui.QGroupBox()
+        controls_box.setTitle("Active facial keypoint")
+        controls_layout = QtGui.QHBoxLayout()
+
+        btn = QtGui.QPushButton("PREV",  self.parent)
+        btn.clicked.connect(lambda: self.parent.change_current_keypoint(-1))
+        btn.resize(self.BUTTON_WIDTH,  self.BUTTON_HEIGHT)
+        controls_layout.addWidget(btn)
+
+        btn = QtGui.QPushButton("NEXT",  self.parent)
+        btn.clicked.connect(lambda:  self.parent.change_current_keypoint(1))
+        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
+        controls_layout.addWidget(btn)
+
+        btn = QtGui.QPushButton("CLEAR KEYPOINT",  self.parent)
+        btn.clicked.connect(self.parent.clear_current_keypoint)
+        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
+        controls_layout.addWidget(btn)
+
+        controls_box.setLayout(controls_layout)
+        main_keypoints_layout.addWidget(controls_box)
+
+        # add panel with keypoints data lineedits
+        points_box = QtGui.QGroupBox()
+        points_layout = QtGui.QFormLayout()
+
+        for keypoint in self.parent.KEYPOINT_NAMES:
+            points_layout.addRow(keypoint, 
+                self.keypoints_line_edits[keypoint])
+       
+        points_box.setLayout(points_layout)
+        main_keypoints_layout.addWidget(points_box)
+
+    def init_picture_canvas_layout(self, picture_layout):
+        """ initiates layaut of picture box """
+        self.parent.init_plot_objects()
+        picture_layout.addWidget(self.parent.canvas)
+        keypoints_box = QtGui.QGroupBox()
+        picture_layout.addWidget(keypoints_box)  
+
--- faceFeaturesMarker.py
+++ faceFeaturesMarker.py
@@ -35,14 +35,14 @@ class faceFeaturesMarker(QtGui.QMainWindow):
         "MOUTH_RIGHT",
         "MOUTH_TOP",
         "MOUTH_DOWN"]
-    # fixed height of all buttons in app
-    BUTTON_HEIGHT = 25
-    # fixed width of all buttons in app
-    BUTTON_WIDTH = 125
+
     # number of people in dataset
+    # todo move to data loader
     NUMER_OF_PEOPLE = 40
     # number of images of every person
+    # todo move to data loader
     IMAGES_OF_PERSON = 10
+
     # stylesheet of active form with facial keypoint location
     ACTIVE_LINE_EDIT_STYLESHEET = ("color: blue;"
                                     "border-style: outset;"
@@ -51,7 +51,7 @@ class faceFeaturesMarker(QtGui.QMainWindow):
                                     "border-color: yellow;"
                                     "selection-background-color: blue;")
     # path to csv file that is dataset, saves and loads are made to/from this file
-    DATA_FILE_PATH = "C:/scisoft/orl_faces_keypoints.csv"
+    DATA_FILE_PATH = "C:/Users/iwek/Documents/magisterka\FFM/orl_faces_keypoints.csv"
     # precision of gathered datapoints data
     KEYPOINT_DATA_PRECISSION = 2
 
@@ -65,20 +65,11 @@ class faceFeaturesMarker(QtGui.QMainWindow):
         #setup window
         self.setGeometry(50,50,700,0)
         self.setWindowTitle("Features marker")
-        
+        self.GUIManager = GUIManager(self)
         # currently displayed person and image
         # those values are changes by buttons
         self.current_img = 0
         self.current_person = 0
-
-        # dict of keypoit line edits,they need to be members because we need to change their value
-        # when user clicks on image currently active keypoint line edit is filled with coordinates
-        # currently active keypoint is marked by variable current_keypoint
-        self.keypoints_line_edits = dict()
-        for keypoint in self.KEYPOINT_NAMES: 
-            qLine = QtGui.QLineEdit() 
-            qLine.setDisabled(True)
-            self.keypoints_line_edits[keypoint] = qLine
 
         # currently active facial keypoint (active line edit to writing)
         self.current_keypoint = self.KEYPOINT_NAMES[0]
@@ -103,20 +94,21 @@ class faceFeaturesMarker(QtGui.QMainWindow):
             self.read_data_from_csv()
         except FileNotFoundError:
             self.save_data_to_csv()
-        except Exception:
-            print("Unknown data loading error occured.")   
+        #except Exception:
+        #    print("Unknown data loading error occured.")   
         # fill visible boxes with keypoints data from keypoints_data
         self.fill_current_keypoint_boxes_with_cached_data()
         # load pictures data into memory
         # todo: move it to other class to other file and give it more generic implementation (getData or something) so that it could be used with diffrent datasets
-        self.data_loader = imageDataLoader("C:/Users/Michal/Documents/magisterka/dane/orl_faces/")
+        self.data_loader = imageDataLoader("C:/SciSoft/orl_faces/")
         self.data_loader.load_data()
 
         self.xinch = self.data_loader.resolution[0] / self.DPI + 1
         self.yinch = self.data_loader.resolution[1] / self.DPI + 1
 
         # initialize all widgets in window
-        self.init_widgets()   
+        self.GUIManager.init_widgets()
+
         # plot face on canvas widget            
         self.refresh_plot()
 
@@ -128,156 +120,6 @@ class faceFeaturesMarker(QtGui.QMainWindow):
                 self.keypoints_data[person_num][img_num] = dict()
                 for keypoint in self.KEYPOINT_NAMES:
                     self.keypoints_data[person_num][img_num][keypoint] = tuple()
-
-    # todo make better panel split
-    def init_widgets(self):
-        """ Initialize all widgets and put them in right possitions in the main window."""
-        # main box that holds all subboxes
-        main_box = QtGui.QGroupBox()
-        # layaut of main box 
-        main_layout = QtGui.QHBoxLayout()
-        
-        # setup box for buttons in leftmost panel
-        main_menu_group_box = QtGui.QGroupBox()
-        main_menu_layout = QtGui.QVBoxLayout()
-        self.init_main_menu_layout(main_menu_layout)
-        main_menu_group_box.setLayout(main_menu_layout)
-        main_layout.addWidget(main_menu_group_box)
-
-        # setup box keypoints menu
-        keypoints_menu_box =  QtGui.QGroupBox()
-        keypoints_menu_layout = QtGui.QVBoxLayout();
-        self.init_keypoint_menu_layout(keypoints_menu_layout)
-        keypoints_menu_box.setLayout(keypoints_menu_layout)
-        main_layout.addWidget(keypoints_menu_box)
-
-        # setup box for picure
-        picture_group_box =  QtGui.QGroupBox()
-        picture_layout = QtGui.QVBoxLayout();
-        self.init_picture_canvas_layout(picture_layout)
-        picture_group_box.setLayout(picture_layout)
-        main_layout.addWidget(picture_group_box)
-        
-
-        main_box.setLayout(main_layout)
-
-        self.setCentralWidget(main_box)
-
-    def init_main_menu_layout(self, main_menu_layout):
-        """ Initiates main menu layout. """
-        ####
-        # image swap
-        ####
-          
-        img_swap_layout = QtGui.QHBoxLayout()
-        # next img
-        btn = QtGui.QPushButton("PREV", self)
-        btn.clicked.connect(lambda: self.change_potted_img(-1))
-        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
-        img_swap_layout.addWidget(btn)
-
-        # next img
-        btn = QtGui.QPushButton("NEXT", self)
-        btn.clicked.connect(lambda: self.change_potted_img(1))
-        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
-        img_swap_layout.addWidget(btn)
-
-        img_swap_box = QtGui.QGroupBox()
-        img_swap_box.setTitle("Change image")
-        img_swap_box.setLayout(img_swap_layout)
-        main_menu_layout.addWidget(img_swap_box)
-
-
-        #####
-        # person swap
-        #####
-        person_swap_layout = QtGui.QHBoxLayout()
-        
-        # prev person
-        btn = QtGui.QPushButton("PREV", self)
-        btn.clicked.connect(lambda: self.change_plotted_person(-1))
-        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
-        person_swap_layout.addWidget(btn)
-
-        spacerItem = QtGui.QSpacerItem(20, 30, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
-        main_menu_layout.addItem(spacerItem)
-
-        # next person
-        btn = QtGui.QPushButton("NEXT", self)
-        btn.clicked.connect(lambda: self.change_plotted_person(1))
-        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
-        person_swap_layout.addWidget(btn)
-
-        person_swap_box = QtGui.QGroupBox()
-        person_swap_box.setTitle("Change person")
-        person_swap_box.setLayout(person_swap_layout)
-        main_menu_layout.addWidget(person_swap_box)
-
-        spacerItem = QtGui.QSpacerItem(20, 30, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
-        main_menu_layout.addItem(spacerItem)
-
-        # buttons to save data to file
-        btn = QtGui.QPushButton("SAVE",self)
-        btn.clicked.connect(self.save_data_to_csv)
-        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
-        main_menu_layout.addWidget(btn)
-
-        # resets all keypoints on current image
-        btn = QtGui.QPushButton("RESET",self)
-        btn.clicked.connect(self.reset_all_current_img_keypoints)
-        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
-        main_menu_layout.addWidget(btn)
-
-        # quit app
-        btn = QtGui.QPushButton("QUIT",self)
-        btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        main_menu_layout.addWidget(btn)
-
-        # spacer item to squeeze all buttons to the top
-        spacerItem = QtGui.QSpacerItem(20, 100, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        main_menu_layout.addItem(spacerItem)
-
-    def init_keypoint_menu_layout(self, main_keypoints_layout):
-        """ Initiates layout of keypoints menu."""
-        # add control buttons
-        controls_box = QtGui.QGroupBox()
-        controls_box.setTitle("Active facial keypoint")
-        controls_layout = QtGui.QHBoxLayout()
-
-        btn = QtGui.QPushButton("PREV", self)
-        btn.clicked.connect(lambda: self.change_current_keypoint(-1))
-        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
-        controls_layout.addWidget(btn)
-
-        btn = QtGui.QPushButton("NEXT", self)
-        btn.clicked.connect(lambda: self.change_current_keypoint(1))
-        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
-        controls_layout.addWidget(btn)
-
-        btn = QtGui.QPushButton("CLEAR KEYPOINT", self)
-        btn.clicked.connect(self.clear_current_keypoint)
-        btn.resize(self.BUTTON_WIDTH, self.BUTTON_HEIGHT)
-        controls_layout.addWidget(btn)
-
-        controls_box.setLayout(controls_layout)
-        main_keypoints_layout.addWidget(controls_box)
-
-        # add panel with keypoints data lineedits
-        points_box = QtGui.QGroupBox()
-        points_layout = QtGui.QFormLayout()
-
-        for keypoint in self.KEYPOINT_NAMES:
-            points_layout.addRow(keypoint, self.keypoints_line_edits[keypoint])
-       
-        points_box.setLayout(points_layout)
-        main_keypoints_layout.addWidget(points_box)
-
-    def init_picture_canvas_layout(self, picture_layout):
-        """ initiates layaut of picture box """
-        self.init_plot_objects()
-        picture_layout.addWidget(self.canvas)
-        keypoints_box = QtGui.QGroupBox()
-        picture_layout.addWidget(keypoints_box)  
 
     def init_plot_objects(self):
         """ initiate plot objects - figure, axes and canvas. """
@@ -293,11 +135,12 @@ class faceFeaturesMarker(QtGui.QMainWindow):
             self.change_keypoint_line_edit_active(self.current_keypoint, self.KEYPOINT_NAMES[new_keypoint_index])
             self.current_keypoint = self.KEYPOINT_NAMES[new_keypoint_index]
             self.refresh_plot() 
+
     def change_keypoint_line_edit_active(self, old_keypoint, new_keypoint):
         """ marks box of current keypoint as active and deactivates old one"""    
         """ for now just changes styleSheet """
-        self.keypoints_line_edits[old_keypoint].setStyleSheet("")
-        self.keypoints_line_edits[new_keypoint].setStyleSheet(self.ACTIVE_LINE_EDIT_STYLESHEET)    
+        self.GUIManager.keypoints_line_edits[old_keypoint].setStyleSheet("")
+        self.GUIManager.keypoints_line_edits[new_keypoint].setStyleSheet(self.ACTIVE_LINE_EDIT_STYLESHEET)    
 
     def change_plotted_person(self, move):
         """ changes currently plotted person """
@@ -331,7 +174,7 @@ class faceFeaturesMarker(QtGui.QMainWindow):
                             ";" + 
                             str(self.keypoints_data[self.current_person][self.current_img][keypoint][1])
                             )
-            self.keypoints_line_edits[keypoint].setText(new_text)
+            self.GUIManager.keypoints_line_edits[keypoint].setText(new_text)
 
     def refresh_plot(self):
         """ Refreshes plot data and starts pooling events """
@@ -363,8 +206,6 @@ class faceFeaturesMarker(QtGui.QMainWindow):
                 else:
                     plt.scatter([self.keypoints_data[self.current_person][self.current_img][keypoint][0]],
                         self.keypoints_data[self.current_person][self.current_img][keypoint][1], color='blue')
-       
-
 
     def process_plot_mouse_click(self, event):
         """ Processes mouse cli"""
@@ -374,7 +215,7 @@ class faceFeaturesMarker(QtGui.QMainWindow):
             return
         if ix < 0 or iy < 0 or ix > self.data_loader.resolution[0] or iy > self.data_loader.resolution[1]:
             return
-        self.keypoints_line_edits[self.current_keypoint].setText(str(ix) + ";" + str(iy))
+        self.GUIManager.keypoints_line_edits[self.current_keypoint].setText(str(ix) + ";" + str(iy))
         self.keypoints_data[self.current_person][self.current_img][self.current_keypoint] = (ix,iy,)
         self.change_current_keypoint(1)
 
@@ -390,7 +231,7 @@ class faceFeaturesMarker(QtGui.QMainWindow):
         """ Removes current keypoint data from cache keypoints_data container, 
             from plot and from lineEdit """
         self.keypoints_data[self.current_person][self.current_img][self.current_keypoint] = tuple()
-        self.keypoints_line_edits[self.current_keypoint].setText("")
+        self.GUIManager.keypoints_line_edits[self.current_keypoint].setText("")
         self.refresh_plot()        
 
     def reset_all_current_img_keypoints(self):
@@ -399,7 +240,7 @@ class faceFeaturesMarker(QtGui.QMainWindow):
             also refreshes img to clear overlaying scatter plot """
         for keypoint in self.KEYPOINT_NAMES:
             self.keypoints_data[self.current_person][self.current_img][keypoint] = tuple()
-            self.keypoints_line_edits[keypoint].setText("")
+            self.GUIManager.keypoints_line_edits[keypoint].setText("")
             # to setup active keypoint line edit styleSheet
             self.change_current_keypoint(-self.KEYPOINT_NAMES.index(self.current_keypoint))
         self.refresh_plot()
@@ -410,6 +251,13 @@ class faceFeaturesMarker(QtGui.QMainWindow):
         """
         with open(self.DATA_FILE_PATH, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            
+            coor_names = ('X','Y')
+            keypoints_column_names = [keypoint + '_' + coor 
+                                for keypoint in self.KEYPOINT_NAMES 
+                                for coor in coor_names]
+            writer.writerow(['person','image'] + keypoints_column_names)                           
+
             for person in range(self.NUMER_OF_PEOPLE):
                 for image in range(self.IMAGES_OF_PERSON):
                     keypoints_list = list()
@@ -421,12 +269,14 @@ class faceFeaturesMarker(QtGui.QMainWindow):
                             keypoints_list += [i for i in self.keypoints_data[person][image][keypoint]]
                     writer.writerow([person,image] + keypoints_list)
 
+
     def read_data_from_csv(self):
         """ read data from csv file (pointed by DATA_FILE_PATH)
             to keypoints_data structure
         """
         with open(self.DATA_FILE_PATH, 'r', newline='') as csvfile:
             reader = csv.reader(csvfile, delimiter=',',quotechar='|')
+            next(reader) # skip column names
             for row in reader:
                 for keypoint_index in range(len(self.KEYPOINT_NAMES)):
                     if 2 * keypoint_index + 2 < len(row):  
@@ -436,8 +286,12 @@ class faceFeaturesMarker(QtGui.QMainWindow):
                         else:
                             new_tuple = double(new_tuple)            
                         self.keypoints_data[int(row[0])][int(row[1])][self.KEYPOINT_NAMES[keypoint_index]] = new_tuple
+        
     
- 
+
+
+
+
 if __name__ == '__main__':
     qApp = QtGui.QApplication(sys.argv)
     MainWindow = faceFeaturesMarker()
